8b8204ea443d66a14b315af44462ea92e794e6da
go4retro <go4retro@go4retro.com>
2018-06-28 19:50:45 -0500

    v3 HDL

---
+++ README.md
@@ -0,0 +1,2 @@
+# Fake6509
+MOS 6502 to MOS 6509 Adapter
--- hdl/Fak6509.v
+++ hdl/Fak6509.v
@@ -24,17 +24,19 @@
 module Fake6509(input _reset,
                 input phi1_6509,
                 input phi2_6509,
-                output phi2_6502,
                 input r_w,
-                input [15:0]address_cpu,
-                inout [7:0]data_cpu,
-                input rdy,
+                input [15:0]address_6502,
+                inout [7:0]data_6502,
+                output [15:13]address_6509,
+                inout [7:0]data_6509,
+                input _rdy,
                 input sync,
                 output [3:0]address_bank,
-                output [7:0]test,
-                input clock
+                output [6:0]test
                );
  
+reg [7:0]data_6502_out;
+reg [7:0]data_6509_out;
 wire [3:0]data_0000;
 wire [3:0]data_0001;
 wire [1:0]data_cycle1;
@@ -47,7 +49,6 @@ wire ce_bank;
 wire oe_bank;
 wire we_bank;
 
-
 assign test[0] = 0;
 assign test[1] = 0;
 assign test[2] = 0;
@@ -55,38 +56,56 @@ assign test[3] = 0;
 assign test[4] = 0;
 assign test[5] = 0;
 assign test[6] = 0;
-assign test[7] = 0;
-
-assign phi2_6502 =                        phi1_6509;
 
 /* This Verilog attempts to implement the circuit by Dr. Jefyll in this post:
    http://forum.6502.org/viewtopic.php?p=17597&sid=0966e1fa047d491a969a4693b5fed5fd#p17597
 */
 
-assign ce_bank =                          address_cpu[15:1] == 0;
+assign ce_bank =                          address_6502[15:1] == 0;
 assign we_bank =                          ce_bank & !r_w;assign oe_bank =                          r_w & ce_bank;
 
+assign data_6509 =                        data_6509_out;
+assign data_6502 =                        data_6502_out;
+assign address_6509 =                     address_6502[15:13];
+
 // Normal bank register (called Execution bank in MOS documentation)
-register #(.WIDTH(4), .RESET(4'b1111))    reg_0000(phi2_6502, !_reset, we_bank & !address_cpu[0], data_cpu[3:0], data_0000);
+register #(.WIDTH(4), .RESET(4'b1111))    reg_0000(phi2_6509, !_reset, we_bank & !address_6502[0], data_6502[3:0], data_0000);
 // Indirect bank register (used for LDA Indirect, Y and STA Indirect, Y)
-register #(.WIDTH(4), .RESET(4'b1111))    reg_0001(phi2_6502, !_reset, we_bank & address_cpu[0], data_cpu[3:0], data_0001);
+register #(.WIDTH(4), .RESET(4'b1111))    reg_0001(phi2_6509, !_reset, we_bank & address_6502[0], data_6502[3:0], data_0001);
 // store copy of various important signals.  
-register #(.WIDTH(2))                     reg_opcode(phi2_6502, !_reset, rdy, {sync & data_cpu[7] & !data_cpu[6] & (data_cpu[4:0] == 5'b10001), address_cpu[0]}, data_cycle1);
+register #(.WIDTH(2))                     reg_opcode(phi2_6509, !_reset, _rdy, {sync & data_6502[7] & !data_6502[6] & (data_6502[4:0] == 5'b10001), address_6502[0]}, data_cycle1);
 // compute the outcome of our combinatorial decision and store
-register #(.WIDTH(1))                     reg_clock2(phi2_6502, !_reset, rdy, data_cycle1[1] & (data_cycle1[0] ^ address_cpu[0]), data_cycle2);
+register #(.WIDTH(1))                     reg_clock2(phi2_6509, !_reset, _rdy, data_cycle1[1] & (data_cycle1[0] ^ address_6502[0]), data_cycle2);
 // shift
-register #(.WIDTH(1))                     reg_clock3(phi2_6502, !_reset, rdy, data_cycle2, data_cycle3);
+register #(.WIDTH(1))                     reg_clock3(phi2_6509, !_reset, _rdy, data_cycle2, data_cycle3);
 // shift
-register #(.WIDTH(1))                     reg_clock4(phi2_6502, !_reset, rdy, data_cycle3, data_cycle4);
+register #(.WIDTH(1))                     reg_clock4(phi2_6509, !_reset, _rdy, data_cycle3, data_cycle4);
 // shift
-register #(.WIDTH(1))                     reg_clock5(phi2_6502, !_reset, rdy, data_cycle4, data_cycle5);
+register #(.WIDTH(1))                     reg_clock5(phi2_6509, !_reset, _rdy, data_cycle4, data_cycle5);
 // bank selection
 assign sel_bank =                         (data_cycle5 & !sync) | data_cycle4;
-//assign address_bank =                     ( sel_bank ? data_0001 : data_0000);
-assign address_bank =                     (ce_bank & r_w ? 4'hf: ( sel_bank ? data_0001 : data_0000));
+
+assign address_bank =                     ( sel_bank ? data_0001 : data_0000);
 // read bank registers in any bank.
-wire [3:0]data_bank =                     (address_cpu[0] ? data_0001 : data_0000);
-assign data_cpu =                         (oe_bank  ? {4'b0000,data_bank} : 8'bz);
+wire [3:0]data_bank =                     (address_6502[0] ? data_0001 : data_0000);
+
+always @(*)
+begin
+   if(oe_bank)
+      data_6502_out = {4'b0000, data_bank};
+   else if(r_w & _rdy & phi2_6509)
+      data_6502_out = data_6509;
+   else
+      data_6502_out = 8'bz;
+end
+
+always @(*)
+begin
+   if(!r_w & _rdy & phi2_6509)
+      data_6509_out = data_6502;
+   else
+      data_6509_out = 8'bz;
+end
 
 endmodule
 
